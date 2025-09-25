df45aa447760d20265c07f1d265b0f562063ab97
EjazRay <raymane@sheridancollege.ca>
2025-09-24 22:03:26 -0400

    practice 1

---
+++ src/pract_week1_week3/Inventory.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pract_week1_week3;
+
+/**
+ *
+ * @author ejazr
+ */
+public class Inventory {
+    
+    private long id;
+    private String name; 
+    private String cat; 
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+    
+    
+}
---
+++ src/pract_week1_week3/Pract_Week1_Week3.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pract_week1_week3;
+
+/**
+ *
+ * @author ejazr
+ */
+public class Pract_Week1_Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Inventory inv[] = new Inventory[3]; 
+        
+        Inventory inv1 = new Inventory();
+        inv1.setId(12); 
+        inv1.setName("Pen");
+        inv1.setCat("Pens");
+        inv[0] = (inv1);
+        
+        Inventory inv2 = new Inventory();
+        inv2.setId(13); 
+        inv2.setName("Pencil");
+        inv2.setCat("Pencils");
+        
+        inv[1] = (inv2);
+        
+        
+        Inventory inv3 = new Inventory();
+        inv3.setId(14); 
+        inv3.setName("Marker");
+        inv3.setCat("Markers");
+        
+        inv[2] = (inv3);
+        
+        
+        for(Inventory in:inv){
+            System.out.print(in.getCat() + " " + in.getId() + " " + in.getName() + "\n"); 
+        }
+    }
+    
+}
