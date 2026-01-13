061b52406fb0beacb6608e0df4e8004963c36af3
EjazRay <raymane@sheridancollege.ca>
2026-01-13 13:01:37 -0500

    First change in VS

--- src/pract_week1_week3/Pract_Week1_Week3.java
+++ src/pract_week1_week3/Pract_Week1_Week3.java
@@ -50,7 +50,7 @@ public class Pract_Week1_Week3 {
         for(Inventory in:inv){
             System.out.print(in.getCat() + " " + in.getId() + " " + in.getName() + "\n"); 
         }
-        
+        //I am adding a comment to see how this works 
         
     }
     
